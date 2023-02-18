6fb4fff1aecbd0eed8b3a9b6b1611fb42c2f0969
JieTo <124500694+jieto0xqz@users.noreply.github.com>
2023-02-18 15:52:44 +0300

    Add files via upload

---
+++ named2/src/HomeWork2.java
@@ -0,0 +1,138 @@
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HomeWork2 {
+
+
+    public static List<String> loginList = Arrays.asList(
+            "Minecraft_12",                                     // true
+            "Player_3433",                                      // true
+            "Dok_a111",                                         // true
+            "Java",                                             // false
+            "1122233",                                          // false
+            "Play__",                                           // false
+            "_Sun2_",                                           // true
+            "____",                                             // false
+            "Winx!",                                            // false
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa12_",            // false
+            "WOWOWOOWOWOWOOWOWOWOWOWOW",                        // false
+            "Correct_22"                                        // true
+    );
+
+
+    public static List<Boolean> checkLoginResults = Arrays.asList(
+            true, true, true, false, false, false, true, false, false, false, false, true
+    );
+
+    public static void main(String[] args) {
+        System.out.println("\nTests for validateLogin");
+        AntiCheat.run();
+        for (int i = 0; i < loginList.size(); i++) {
+            try {
+                validateLogin(loginList.get(i));
+                printTestCase(i, checkLoginResults.get(i), true, 20);
+            } catch(Exception e) {
+                printTestCase(i, checkLoginResults.get(i), false, 20);
+            }
+        }
+
+        System.out.println("\nTests for isLoginValid");
+        AntiCheat.run();
+        for (int i = 0; i < loginList.size(); i++)
+            printTestCase(i + loginList.size(),
+                    checkLoginResults.get(i),
+                    isLoginValid(loginList.get(i)),
+                    20);
+    }
+
+
+    public static void validateLogin(String login) throws LoginValidationException {
+        int lower = 0, upper = 0, under = 0, number = 0;
+        for (String symbol : login.split("")) {
+            char ch = symbol.charAt(0);
+            if (symbol.equals("_")) {
+                under++;
+            } else if (Character.isDigit(ch)) {
+                number++;
+            } else if (Character.isLowerCase(ch)) {
+                lower++;
+            } else if (Character.isUpperCase(ch)) {
+                upper++;
+            }
+        }
+
+        Pattern p = Pattern.compile("^[a-zA-Z0-9_]*[0-9]*$");
+        boolean is_contains = lower >= 1 && upper >= 1 && under >= 1 && number >= 1;
+        if (login.length() <= 20 && p.matcher(login).find() && is_contains) {
+
+        } else {
+            throw new LoginValidationException();
+        }
+    }
+
+    public static Boolean isLoginValid(String login)  {
+        try {
+           validateLogin(login);
+            return true;
+        } catch (LoginValidationException e) {
+            return false;
+        }
+
+    }
+
+    public static class AntiCheat {
+        public static void run() {
+            StringBuilder sb = new StringBuilder("");
+            List<String> antiCheatList = new ArrayList<>();
+            antiCheatList.addAll(loginList);
+            antiCheatList.addAll(checkLoginResults.stream().map(Object::toString).toList());
+            antiCheatList.add(sb.toString());
+            calcHash(antiCheatList);
+        };
+
+        public static String bytesToHex(byte[] bytes) {
+            char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+            char[] hexChars = new char[bytes.length * 2];
+            for (int j = 0; j < bytes.length; j++) {
+                int v = bytes[j] & 0xFF;
+                hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+                hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+            }
+            return new String(hexChars);
+        }
+
+        public static void calcHash(List<String> list) {
+            String total = String.join("", list);
+            try {
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(total.getBytes());
+                byte[] digest = md.digest();
+                System.out.println("AntiCheatCheck: " + bytesToHex(digest));
+            } catch (NoSuchAlgorithmException ignored) {}
+        }
+    }
+
+    public static String constLen(String str, int len) {
+        StringBuilder sb = new StringBuilder(str);
+        while (len-- - str.length() > 0) sb.append(" ");
+        return sb.toString();
+    }
+
+    public static void printTestCase(int n, Boolean exp, Boolean act, int minLen) {
+        Function<String, String> green = str -> "\u001B[34m" + str + "\u001B[0m";
+        Function<String, String> yellow = str -> "\u001B[33m" + str + "\u001B[0m";
+        System.out.print( "TEST CASE " + constLen(String.valueOf(n), 4));
+        System.out.print( "Ожидание: " + yellow.apply(constLen(exp.toString(), minLen)) + " Реальность: " + green.apply(constLen(act.toString(), minLen) + " "));
+        if (Objects.equals(exp, act)) System.out.print("✅"); else System.out.print("❌");
+        System.out.println();
+    }
+
+}
---
+++ named2/src/LoginValidationException.java
@@ -0,0 +1,7 @@
+public class LoginValidationException extends Exception{
+    LoginValidationException() {
+        super("Ошибка, логин не соответствует требованиям!");
+
+    }
+
+}
